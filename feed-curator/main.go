9ef313b84719686c1c7ec8283c202fbd22e9972d
PythonHacker24 <adityapatil24680@gmail.com>
2024-11-08 17:12:02 +0530

    started working on gemini api

--- feed-curator/main.go
+++ feed-curator/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
     "fmt"
+    "log"
+    "context"
+    "os"
+
+    "github.com/google/generative-ai-go/genai"
+    "google.golang.org/api/option"
+
 )
 
 func main() {
-    fmt.Println("Hello Curator!")
+
+    ctx := context.Background()
+    // Access your API key as an environment variable (see "Set up your API key" above)
+    client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("API_KEY")))
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer client.Close()
+
+    model := client.GenerativeModel("gemini-1.5-flash")
+    resp, err := model.GenerateContent(ctx, genai.Text("Write a story about a magic backpack."))
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("Printing Response")
+    fmt.Println(resp)
 }
